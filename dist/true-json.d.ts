cdcc216bcf985ba0ae19f730b77bb3fd73bea4e5
nestorrente <nestorrente@gmail.com>
2022-02-22 14:38:23 +0100

    Updated build.

--- dist/true-json.d.ts
+++ dist/true-json.d.ts
@@ -20,17 +20,8 @@ export declare class JsonConverter<T> {
 	stringify(value: T, space?: string | number): string;
 	parse(text: string): T;
 }
-export declare type MapEntry<K, V> = [
-	K,
-	V
-];
-export interface MapAdapterConfig<K, V, JK extends JsonValue = JsonValue, JV extends JsonValue = JsonValue> {
-	keyAdapter: JsonAdapter<K, JK>;
-	valueAdapter: JsonAdapter<V, JV>;
-}
-export declare type StringKeyOf<T> = string & keyof T;
-export declare type TypeAssertion<T> = (value: unknown) => asserts value is T;
-declare function getIdentityAdapter<T extends JsonValue = JsonValue>(typeChecksCallback?: TypeAssertion<T>): JsonAdapter<T, T>;
+export declare type InputValueValidator = (value: unknown) => void;
+declare function getIdentityAdapter<T extends JsonValue = JsonValue>(validator?: InputValueValidator): JsonAdapter<T, T>;
 declare function getISODateAdapter(): JsonAdapter<Date, string>;
 declare function getDateTimestampAdapter(): JsonAdapter<Date, number>;
 declare function getArrayJsonAdapter<T, U extends JsonValue = JsonValue>(elementAdapter: JsonAdapter<T, U>): JsonAdapter<T[], JsonArray<U>>;
@@ -40,6 +31,14 @@ export interface RecordAdapterConfig {
 	strictPlainObjectCheck: boolean;
 }
 declare function getRecordAdapter<T, U extends JsonValue = JsonValue>(valueAdapter: JsonAdapter<T, U>, config?: RecordAdapterConfig): JsonAdapter<Record<string, T>, JsonObject<U>>;
+export declare type MapEntry<K, V> = [
+	K,
+	V
+];
+export interface MapAdapterConfig<K, V, JK extends JsonValue = JsonValue, JV extends JsonValue = JsonValue> {
+	keyAdapter: JsonAdapter<K, JK>;
+	valueAdapter: JsonAdapter<V, JV>;
+}
 declare function getMapAsEntriesAdapter<K, V, JK extends JsonValue = JsonValue, JV extends JsonValue = JsonValue>(config?: Partial<MapAdapterConfig<K, V, JK, JV>>): JsonAdapter<Map<K, V>, JsonArray<MapEntry<JK, JV>>>;
 declare function getMapAsRecordAdapter<K, V, JV extends JsonValue = JsonValue>(config?: Partial<MapAdapterConfig<K, V, string, JV>>): JsonAdapter<Map<K, V>, JsonObject<JV>>;
 export declare type PropertyAdapters<T> = {
@@ -52,6 +51,7 @@ export interface ObjectAdapterConfig<T = unknown> {
 	omittedProperties: (keyof T)[];
 }
 declare function getObjectAdapter<T>(propertyAdapters: PropertyAdapters<T>, config?: Partial<ObjectAdapterConfig<T>>): JsonAdapter<T, JsonObject>;
+export declare type StringKeyOf<T> = string & keyof T;
 declare function getByKeyAdapter<T, R extends Record<string, T>>(keyValuePairs: R): JsonAdapter<T, StringKeyOf<R>>;
 declare function getByKeyLenientAdapter<T, R extends Record<string, T> = Record<string, T>>(keyValuePairs: R): JsonAdapter<T | undefined, StringKeyOf<R> | undefined>;
 declare function getByKeyLenientAdapter<T, R extends Record<string, T> = Record<string, T>>(keyValuePairs: R, fallbackKey: StringKeyOf<R>): JsonAdapter<T, StringKeyOf<R>>;
@@ -59,8 +59,16 @@ declare function getCustomAdapter<T, U extends JsonValue = JsonValue>(adapter: J
 declare function getNullishAwareCustomAdapter<T, U extends JsonValue = JsonValue>(adapter: JsonAdapter<T, U>): JsonAdapter<T | null | undefined, U | null | undefined>;
 declare function getUndefinedAwareCustomAdapter<T, U extends JsonValue = JsonValue>(adapter: JsonAdapter<T, U>): JsonAdapter<T | undefined, U | undefined>;
 declare function getNullAwareCustomAdapter<T, U extends JsonValue = JsonValue>(adapter: JsonAdapter<T, U>): JsonAdapter<T | null, U | null>;
+declare function getStringIdentityAdapter(): JsonAdapter<string, string>;
+declare function getNumberIdentityAdapter(): JsonAdapter<number, number>;
+declare function getIntegerIdentityAdapter(): JsonAdapter<number, number>;
+declare function getBooleanIdentityAdapter(): JsonAdapter<boolean, boolean>;
 export declare const JsonAdapters: {
 	identity: typeof getIdentityAdapter;
+	stringIdentity: typeof getStringIdentityAdapter;
+	numberIdentity: typeof getNumberIdentityAdapter;
+	integerIdentity: typeof getIntegerIdentityAdapter;
+	booleanIdentity: typeof getBooleanIdentityAdapter;
 	isoDate: typeof getISODateAdapter;
 	dateTimestamp: typeof getDateTimestampAdapter;
 	array: typeof getArrayJsonAdapter;
